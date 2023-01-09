719ad268dfda6ffe167753e16232bb6213ecdc1e
Maksim Remin <reminmax@gmail.com>
2023-01-09 02:35:11 +0200

    Added handler for "validate" command line argument

---
+++ PasswordValidatorApp/CmdLineArgsParser/ValidateOptions.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine.Text;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidatorApp.CmdLineArgsParser
+{
+    [Verb("validate", HelpText = "Validate passwords in batch mode")]
+    internal class ValidateOptions
+    {
+        [Option(
+            shortName: 'f',
+            longName: "filename",
+            Required = true,
+            HelpText = "Set the path to the file containing passwords to validate"
+            )]
+
+        public string Filename { get; set; } = "";
+    }
+}
