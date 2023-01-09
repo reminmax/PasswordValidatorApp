9d690884efe7047ac1bbf44e74d71cd8ac8bc437
Maksim Remin <reminmax@gmail.com>
2023-01-09 02:51:29 +0200

    Added entities

---
+++ PasswordValidatorApp/Data/Entities/ValidationData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidatorApp.Data.Entities
+{
+    public struct ValidationData
+    {
+        public char SearchCharacter { get; private set; }
+        public byte MinimumNumberOfOccurrences { get; private set; }
+        public byte MaximumNumberOfOccurrences { get; private set; }
+        public string PasswordString { get; private set; }
+
+        public ValidationData(
+            char searchCharacter,
+            byte minimumNumberOfOccurrences,
+            byte maximumNumberOfOccurrences,
+            string passwordString)
+        {
+            SearchCharacter = searchCharacter;
+            MinimumNumberOfOccurrences = minimumNumberOfOccurrences;
+            MaximumNumberOfOccurrences = maximumNumberOfOccurrences;
+            PasswordString = passwordString;
+        }
+    }
+}
---
+++ PasswordValidatorApp/Data/Entities/ValidationResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidatorApp.Data.Entities
+{
+    public class ValidationResult
+    {
+        private List<string> _errors;
+        private List<string> _validPasswords;
+
+        internal ValidationResult()
+        {
+            _errors = new List<string>(0);
+            _validPasswords = new List<string>(0);
+        }
+
+        public bool HasError => _errors.Count > 0;
+
+        public IReadOnlyCollection<string> Errors => _errors;
+        public IReadOnlyCollection<string> ValidPasswords => _validPasswords;
+
+        internal void AddError(string errorMessage)
+        {
+            _errors.Add(errorMessage);
+        }
+
+        internal void AddValidPassword(string password)
+        {
+            _validPasswords.Add(password);
+        }
+    }
+}
