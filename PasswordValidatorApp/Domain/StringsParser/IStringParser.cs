789c58c5fc1ad6f81a45f1716e308869e28fc976
Maksim Remin <reminmax@gmail.com>
2023-01-09 02:55:24 +0200

    Added class to parse strings retrieved from the repository

---
+++ PasswordValidatorApp/Domain/StringsParser/IStringParser.cs
@@ -0,0 +1,16 @@
+﻿using PasswordValidatorApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidatorApp.Domain.StringsParser
+{
+    public interface IStringParser
+    {
+        bool IsStringMatchesPattern(string input);
+
+        ValidationData GetValidationData(string input);
+    }
+}
---
+++ PasswordValidatorApp/Domain/StringsParser/StringParser.cs
@@ -0,0 +1,22 @@
+﻿using PasswordValidatorApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidatorApp.Domain.StringsParser
+{
+    public class StringParser : IStringParser
+    {
+        public ValidationData GetValidationData(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsStringMatchesPattern(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
