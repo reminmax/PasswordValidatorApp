8281532e7d43c17c7c664a32850ff0215ebd1435
Maksim Remin <reminmax@gmail.com>
2023-01-09 03:44:37 +0200

    Added a class that encapsulates parsing and validation logic

---
+++ PasswordValidatorApp/Domain/MainProcessor.cs
@@ -0,0 +1,34 @@
+﻿using PasswordValidatorApp.Data.Entities;
+using PasswordValidatorApp.Data.Repository;
+using PasswordValidatorApp.Domain.StringsParser;
+using PasswordValidatorApp.Domain.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidatorApp.Domain
+{
+    public class MainProcessor
+    {
+        private readonly IStringParser _stringParser;
+        private readonly IPswValidator _validator;
+        private readonly IRepository _repository;
+
+        public MainProcessor(
+            IRepository repository,
+            IStringParser stringParser,
+            IPswValidator validator)
+        {
+            _stringParser = stringParser;
+            _validator = validator;
+            _repository = repository;
+        }
+
+        public async Task<ValidationResult> GetValidationResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
