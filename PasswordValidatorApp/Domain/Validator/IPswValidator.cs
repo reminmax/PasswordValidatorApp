12eab9b5c528d64e58157ec0652e8d5a5ade6644
Maksim Remin <reminmax@gmail.com>
2023-01-09 03:33:03 +0200

    Added class to validate password

---
+++ PasswordValidatorApp/Domain/Validator/IPswValidator.cs
@@ -0,0 +1,14 @@
+﻿using PasswordValidatorApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidatorApp.Domain.Validator
+{
+    public interface IPswValidator
+    {
+        bool CheckIfPasswordValid(ValidationData validationData);
+    }
+}
---
+++ PasswordValidatorApp/Domain/Validator/PswValidator.cs
@@ -0,0 +1,17 @@
+﻿using PasswordValidatorApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidatorApp.Domain.Validator
+{
+    public class PswValidator : IPswValidator
+    {
+        public bool CheckIfPasswordValid(ValidationData validationData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
