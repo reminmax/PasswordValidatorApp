c4677dfe3d1b54eb798294300e598a51efcb0ea2
Maksim Remin <reminmax@gmail.com>
2023-01-09 02:40:02 +0200

    Checking if the file specified as a command line argument is read

--- PasswordValidatorApp/Program.cs
+++ PasswordValidatorApp/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace PasswordValidatorApp
+﻿using CommandLine;
+using PasswordValidatorApp.CmdLineArgsParser;
+
+namespace PasswordValidatorApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            await Parser.Default
+                .ParseArguments<ValidateOptions>(args)
+                .WithParsedAsync(async options => {
+                    if (!string.IsNullOrEmpty(options.Filename))
+                    {
+                        await RunValidation(options.Filename);
+                    }
+                });
+        }
+
+        private static async Task RunValidation(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"File {fileName} doesn't exist. Check the path.");
+                return;
+            }
+
+            try
+            {
+                using (var reader = File.OpenText(fileName))
+                {
+                    var fileText = await reader.ReadToEndAsync();
+
+                    var stringArray = fileText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                    foreach (var item in stringArray)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
